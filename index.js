61756a48f54feae442c98c17a394db9c90a72031
firas-debich <firasdebich06@gmail.com>
2022-07-20 18:02:20 +0100

    upload files and images done

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+const main = () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.post("/upload", upload.single("file"), (req, res) => {
+    const file = req.file.path;
+    res.send(file);
+  });
+  app.post("/upload-multiple", upload.array("files"), (req, res) => {
+    const filesArray = req.files.map(e=>e.path)
+    console.log(req.files);
+    res.json(filesArray);
+  });
+
+  app.listen(process.env.PORT || 3333, (_) => console.log("Server is running"));
+};
+
+main();
